a923a6383762dae0b469c07bbaa8e78a93ba5f4e
Aleksander Sorokin <asorokin@evolution.com>
2023-04-14 22:35:07 +0200

    add tests

--- app/deck.js
+++ app/deck.js
@@ -3,3 +3,9 @@ import { suits } from "./suits.js";
 import { cardOf } from './card.js';
 
 export const deckOfCards = ranks.flatMap((rank) => suits.map((suit) => cardOf(rank, suit)));
+
+function test0() {
+  console.assert(deckOfCards.length === 52);
+}
+
+test0();
--- app/hand.js
+++ app/hand.js
@@ -1,5 +1,7 @@
-import { cardToString } from "./card.js";
+import { cardOf, cardToString } from "./card.js";
 import { Bust, evaluateHand } from "./evaluate-hand.js";
+import { Rank } from "./ranks.js";
+import { Suit } from "./suits.js";
 
 export function handToString(hand) {
   return hand.map(cardToString).join(", ");
@@ -9,3 +11,17 @@ export function evaluatedHandToString(hand) {
   const scores = evaluateHand(hand);
   return `${handToString(hand)} (=${scores === Bust ? "∞" : scores})`;
 }
+
+function test0() {
+  const hand = [
+    cardOf(Rank.ace, Suit.clubs),
+    cardOf(Rank.ace, Suit.spades),
+    cardOf(Rank.ace, Suit.hearts),
+    cardOf(Rank.ace, Suit.diamonds)
+  ];
+  const actual = evaluatedHandToString(hand);
+  const expected = 'Ac, As, Ah, Ad (=14)';
+  console.assert(actual === expected);
+}
+
+test0();
