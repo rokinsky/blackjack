99a8753b7781d6b39cf91768ef7a0ec24eb799a7
Aleksander Sorokin <asorokin@evolution.com>
2023-04-14 20:27:58 +0200

    add ranks and suits

---
+++ app/ranks.js
@@ -0,0 +1,17 @@
+export const Rank = {
+  ace: 'A',
+  king: 'K',
+  queen: 'Q',
+  jack: 'J',
+  ten: 'T',
+  nine: '9',
+  eight: '8',
+  seven: '7',
+  six: '6',
+  five: '5',
+  fourth: '4',
+  three: '3',
+  two: '2',
+};
+
+export const ranks = Object.values(Rank);
