49e66233444b1648aa22b7bde38c5543e0ea8560
Aleksander Sorokin <asorokin@evolution.com>
2023-04-14 21:02:35 +0200

    add hand evaluation

--- app/hand.js
+++ app/hand.js
@@ -1,5 +1,11 @@
 import { cardToString } from "./card.js";
+import { Bust, evaluateHand } from "./evaluate-hand.js";
 
 export function handToString(hand) {
   return hand.map(cardToString).join(", ");
+}
+
+export function evaluatedHandToString(hand) {
+  const scores = evaluateHand(hand);
+  return `${handToString(hand)} (=${scores === Bust ? "∞" : scores})`;
 }
