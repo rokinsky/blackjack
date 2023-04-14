da839fcb207c4e1aef03d3d69d962f0a589f4cf4
Aleksander Sorokin <asorokin@evolution.com>
2023-04-14 20:46:46 +0200

    add hand

--- app/main.js
+++ app/main.js
@@ -1,8 +1,14 @@
 import { deckOfCards } from "./deck.js";
-import { prettify } from "./lib/prettify.js";
+import { handToString } from "./hand.js";
 
 function main() {
-  alert(prettify(deckOfCards));
+  const card0 = deckOfCards[0];
+  const card1 = deckOfCards[1];
+  const card2 = deckOfCards[2];
+  const card3 = deckOfCards[3];
+  const hand = [card0, card1, card2, card3];
+
+  alert(handToString(hand));
 }
 
 main();
