6f6d98e96d2c06c0c5c537b77e5bbb463e332789
Aleksander Sorokin <asorokin@evolution.com>
2023-04-14 22:53:27 +0200

    add dealer gameplay (#7)

--- app/main.js
+++ app/main.js
@@ -1,16 +1,28 @@
 import { deckOfCards } from "./deck.js";
+import { Bust, evaluateHand } from "./evaluate-hand.js";
 import { evaluatedHandToString } from "./hand.js";
-import { play } from "./play.js";
+import { dealerHandMessage } from "./messages.js";
+import { dealerHitOrStand, play, playerHitOrStand } from "./play.js";
 
 function main() {
   const deck = [...deckOfCards];
 
+  let dealerHand = [deck.pop()];
   let playerHand = [deck.pop(), deck.pop()];
 
-  playerHand = play(deck, playerHand);
+  alert(dealerHandMessage(dealerHand));
 
-  const result = evaluatedHandToString(playerHand);
-  alert(result);
+  playerHand = play(deck, playerHand, playerHitOrStand);
+
+  if (evaluateHand(playerHand) !== Bust) {
+    dealerHand = play(deck, dealerHand, dealerHitOrStand);
+  }
+
+  const playerResult = evaluatedHandToString(playerHand);
+  alert(playerResult);
+
+  const dealerResult = evaluatedHandToString(dealerHand);
+  alert(dealerResult);
 }
 
 main();
